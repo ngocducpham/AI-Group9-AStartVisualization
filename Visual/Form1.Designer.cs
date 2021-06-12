5eb08c9707b4b002dcb5597fb1880d7525757a6f
october-xxi <19110157@student.hcmute.edu.vn>
2021-06-12 10:11:05 +0700

    change

--- Visual/Form1.Designer.cs
+++ Visual/Form1.Designer.cs
@@ -379,6 +379,7 @@ namespace Visual
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AStart Visualization";
